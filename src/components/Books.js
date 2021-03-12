db1726888eaedce6db24272d37fe1c473c49ab2a
lachicho <mcmoneta@gmail.com>
2021-03-12 09:50:45 -0300

    nivel 1

---
+++ src/components/Books.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Books(){
+    return (
+        <main>
+            <h1>Viaje a la luna</h1>
+            <h3>Julio Verne</h3>
+        </main>
+    )
+}
+
+export default Books
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Books from './components/Books';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
